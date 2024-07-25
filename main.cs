06e07ddf407b5e44afa7d900acad98f98f83945b
K-0de <ochampaughkaleb@outlook.com>
2024-07-25 15:42:46 +0000

    Lyrics 1.2

--- main.cs
+++ main.cs
@@ -2,10 +2,42 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    const double centPerInch = 2.54;
-   Console.WriteLine("Enter a number in inches: ");
-    var x = Convert.ToDouble(Console.ReadLine());
-   
-    Console.WriteLine("{0} inches is {1} centimeters", x, x * centPerInch);
+  int unitTest = Convert.ToInt32(Console.ReadLine());
+  switch(unitTest){
+  case 1: {
+    Console.WriteLine("Oh, well imagine/");
+    Console.WriteLine("As I'm pacing the pews in a church corridor/");
+    Console.WriteLine( "And I can't help but to hear/");
+    Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+       
+    break;
+  }
+  case 2:{
+    Console.WriteLine("Oh-oh-oh-oh-oh,");
+    Console.WriteLine("oh-oh-oh-oh,");
+    Console.WriteLine("oh-oh-oh /");
+    Console.WriteLine("Caught in a bad romance...");
+    break;
+  }
+  case 3:{
+    Console.WriteLine("Coming out of my cage");
+    Console.WriteLine("And I've been doing just fine");
+    Console.WriteLine("Gotta gotta be down");
+    Console.WriteLine("Because I want it all");
+    break;
+  }
+  case 4:{
+    Console.WriteLine("Now, this is a story all about how");
+    Console.WriteLine("My life got flipped-turned upside down");
+    Console.WriteLine("And I'd like to take a minute");
+    Console.WriteLine("Just sit right there");
+    Console.WriteLine("I'll tell you how I became the prince of a town called Bel-Air");
+    
+    break;
+  }
+    
+  }
     }
   }
+    
+    
